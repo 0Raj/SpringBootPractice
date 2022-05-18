0b7761e94111c99d9d96d6756c5e7f2098533776
0Raj <rajasekark0@gmail.com>
2022-05-18 15:12:32 +0530

    practice

--- firstProject/src/com/day2/MySwitchCase.java
+++ firstProject/src/com/day2/MySwitchCase.java
@@ -4,7 +4,20 @@ public class MySwitchCase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 50000;
+		
+		int naturalNumber = n*(n+1)/2;
+		
+		System.out.println(naturalNumber);
+		
+		int sum =0;
+		for(int i=0; i<=n; i++) {
+			sum += i;
+		}
+		//int c = 2147483647;
+		System.out.println(sum);
+		
+		
 	}
 
 }
