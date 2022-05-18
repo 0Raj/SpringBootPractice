6a17ee485ad71a487bbbc42d74fe00be4d914909
0Raj <rajasekark0@gmail.com>
2022-05-18 11:10:25 +0530

    practice

---
+++ firstProject/src/com/day2/Question2.java
@@ -0,0 +1,19 @@
+package com.day2;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+		int run1 = 5;//5
+		int run2 = 6;//12
+		int run3 = 3;//9
+		int run4 = 2;//8
+		int run6 = 1;//1
+		//fun1();
+	}
+	
+	public static void fun1() {
+		System.out.println("test");
+	}
+
+}
---
+++ firstProject/src/com/masai/Main.java
@@ -0,0 +1,12 @@
+package com.masai;
+
+public class Main {
+
+	public static void main(String[] args) {
+			byte a = 117;
+			//byte b = a++;
+		    
+		    System.out.println(++a);
+	}
+
+}
